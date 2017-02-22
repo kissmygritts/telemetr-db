cb7abc03780d8b7b6f7b82d9f2dc083ba19814ff
kissmygritts <kissmygritts08@gmail.com>
2017-02-21 18:09:03 -0800

    testing 2.5 million location upload to database

--- procedures/insert_gps_to_relocations.sql
+++ procedures/insert_gps_to_relocations.sql
@@ -1,5 +1,18 @@
+/* insert gps into relocations where row is between inservice - outservice and not in relocations.gps_id. This should only insert new relocations. */
+
+-- TODO: this needs to be added to the import function for gps data
+
+/* CTE: common table expression acts as a subquery to select a concatenated
+psuedo index that can be checked so that existing records aren't reentered */
+WITH sq AS (
+  SELECT
+    gps.serial_num || relocations.acq_time_lcl AS jnr
+  FROM gps
+    INNER JOIN relocations ON gps.id = relocations.gps_id
+)
+
 INSERT INTO relocations (
-  raw_gps_id,
+  gps_id,
   device_id,
   animal_id,
   acq_time_utc,
@@ -11,24 +24,27 @@ INSERT INTO relocations (
   temperature
 )
 SELECT
-  raw_gps.id AS raw_gps_id,
+  gps.id AS gps_id,
   deployments.device_id,
   deployments.animal_id,
-  raw_gps.acq_time_utc,
-  raw_gps.acq_time_lcl,
-  raw_gps.longitude,
-  raw_gps.latitude,
-  raw_gps.altitude,
-  raw_gps.activity,
-  raw_gps.temperature
-FROM deployments, devices, raw_gps
+  gps.acq_time_utc,
+  gps.acq_time_lcl,
+  gps.longitude,
+  gps.latitude,
+  gps.altitude,
+  gps.activity,
+  gps.temperature
+FROM deployments, devices, gps
 WHERE
-  raw_gps.serial_num = devices.serial_num AND
+  gps.serial_num = devices.serial_num AND
   devices.id = deployments.device_id AND
+  gps.serial_num || gps.acq_time_lcl NOT IN (
+    SELECT jnr FROM sq
+  ) AND
   (
-    (raw_gps.acq_time_lcl >= deployments.inservice AND
-     raw_gps.acq_time_lcl <= deployments.outservice)
+    (gps.acq_time_lcl >= deployments.inservice AND
+     gps.acq_time_lcl <= deployments.outservice)
      OR
-    (raw_gps.acq_time_lcl >= deployments.inservice AND
+    (gps.acq_time_lcl >= deployments.inservice AND
      deployments.outservice IS NULL)
   );
--- seeds/seed_gps.sql
+++ seeds/seed_gps.sql
@@ -9,3 +9,19 @@ COPY gps (
 )
 FROM '/Users/mitchellgritts/Documents/telemetr/telemetr-data/device_030681_test.csv'
 WITH (FORMAT csv, DELIMITER ',', HEADER true);
+
+COPY gps (
+  serial_num,
+  acq_time_lcl,
+  longitude,
+  latitude,
+  activity,
+  temperature,
+  fixtype,
+  hdop,
+  pdop,
+  n_sats,
+  gps_volts
+)
+FROM '/Users/mitchellgritts/Documents/telemetr/telemetr-data/gps.csv'
+WITH (FORMAT csv, DELIMITER ',', HEADER true);
